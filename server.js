5f27410b7c0528052a3a25beee7dd47d8338f34c
lihle-sibiya <129907260+lihle-sibiya@users.noreply.github.com>
2024-02-26 04:45:55 +0200

    10th commit

---
+++ server.js
@@ -0,0 +1,71 @@
+"use strict";
+
+const express = require('express');
+const mongoose = require("mongoose");
+const bcrypt = require('bcrypt');
+
+const app = express();
+const port = 3000;
+
+async function connectToDatabase() {
+    try {
+        await mongoose.connect(
+            process.env.MONGODB_URI || "mongodb+srv://lihle:12345@cluster0.55ttv2e.mongodb.net/html_login"
+        );
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+    }
+}
+
+connectToDatabase();
+
+// Create a schema for user data
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+// Set up middleware to parse JSON
+app.use(express.json());
+
+// Create a model based on the schema
+const UserModel = mongoose.model("User", userSchema);
+
+
+// Serve HTML file
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+// Handle form submissions
+app.post('/', async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        console.log('Received data:', { email, password });
+
+        // Hash password with bcrypt
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+        console.log('Hashed password:', hashedPassword);
+
+        const newUser = new UserModel({
+            email,
+            password: hashedPassword
+        });
+
+        await newUser.save();
+
+        res.json({ success: true, message: 'User created successfully!' });
+    } catch (error) {
+        console.error('Error handling form submission:', error);
+        res.status(400).json({ success: false, message: 'Error creating user' });
+    }
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
