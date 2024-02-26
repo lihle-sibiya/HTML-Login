5628630b7f4c2767a439dd9f933190a7b434da8c
lihle-sibiya <129907260+lihle-sibiya@users.noreply.github.com>
2024-02-26 11:30:03 +0200

    11th commit

--- server.js
+++ server.js
@@ -2,20 +2,23 @@
 
 const express = require('express');
 const mongoose = require("mongoose");
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const app = express();
 const port = 3000;
 
 async function connectToDatabase() {
-    try {
-        await mongoose.connect(
-            process.env.MONGODB_URI || "mongodb+srv://lihle:12345@cluster0.55ttv2e.mongodb.net/html_login"
-        );
-        console.log("Connected to MongoDB");
-    } catch (error) {
-        console.error("Error connecting to MongoDB:", error);
-    }
+try {
+    await mongoose.connect(
+        process.env.MONGODB_URI || "mongodb+srv://lihle:12345@cluster0.c5gdlzz.mongodb.net/html_login",
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+          });
+    console.log('Connected to MongoDB');
+} catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+}
 }
 
 connectToDatabase();
@@ -30,7 +33,7 @@ const userSchema = new mongoose.Schema({
 app.use(express.json());
 
 // Create a model based on the schema
-const UserModel = mongoose.model("User", userSchema);
+const UserModel = mongoose.model("User", userSchema, "users");
 
 
 // Serve HTML file
@@ -38,8 +41,9 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
+
 // Handle form submissions
-app.post('/', async (req, res) => {
+app.post('/users', async (req, res) => {
     const { email, password } = req.body;
 
     try {
@@ -64,7 +68,6 @@ app.post('/', async (req, res) => {
         res.status(400).json({ success: false, message: 'Error creating user' });
     }
 });
-
 // Start the server
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
